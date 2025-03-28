64e1b05381437ce7f78782fc2fe88a735a95f02d
Saad Hadadia <saad.hadadia9@gmail.com>
2025-03-28 11:44:12 +0000

    initialisation du projet

---
+++ KanbanApp/KanbanApp/src/main/java/com/example/KanbanApp/KanbanAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.KanbanApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanAppApplication.class, args);
+	}
+
+}
