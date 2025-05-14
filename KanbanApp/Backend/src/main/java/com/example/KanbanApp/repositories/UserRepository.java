449224ea2e7bf9c18a6f49dc3239ce93ea6c8bc4
Abderrahman DAKIR ALLAH <82895430+black-purple@users.noreply.github.com>
2025-05-14 13:53:50 +0100

    Delete KanbanApp directory

--- KanbanApp/Backend/src/main/java/com/example/KanbanApp/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.KanbanApp.repositories;
-
-import com.example.KanbanApp.models.User.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    public User findByUsername(String username);
-    public User findByEmail(String email);
-}
